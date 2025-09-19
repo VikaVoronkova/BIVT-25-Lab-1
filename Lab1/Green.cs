4bb9b442c6f2038f378637a331d8431c2ae2ebbc
VikaVoronkova <voronkovavika19@gmail.com>
2025-09-19 19:00:02 +0300

    Update Green.cs

--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -5,33 +5,28 @@ namespace Lab1
         public bool Task1(double d)
         {
             bool answer = false;
-
             // code here
             if (Math.Abs(d) >= 1)
             {
                 answer = true;
             }
             // end
-
             return answer;
         }
         public bool Task2(double d, double f)
         {
             bool answer = false;
-
             // code here
             if (((d + f)/2) > 0)
             {
                 answer = true;
             }
             // end
-
             return answer;
         }
         public bool Task3(int a, int b)
         {
             bool answer = false;
-
             // code here
             int mdsr = (Math.Abs(a) + Math.Abs(b))/2;
             if (a+b>mdsr)
@@ -39,13 +34,11 @@ namespace Lab1
                 answer = true;
             }
             // end
-
             return answer;
         }
         public int Task4(int a, int b, int c)
         {
             int answer = 0;
-
             // code here
             if (a > b && a > c)
             {
@@ -60,13 +53,11 @@ namespace Lab1
                 answer = c;
             }
             // end
-
             return answer;
         }
         public double Task5(double x)
         {
             double answer = 0;
-
             // code here
             if (Math.Abs(x) > 1)
             {
@@ -77,27 +68,23 @@ namespace Lab1
                 answer = Math.Pow(x, 2) - 1;
             }
             // end
-
             return answer;
         }
         public bool Task6(double x, double y)
         {
             bool answer = false;
-
             // code here
             if (y >= 0 && y <= 1+x && y <= 1-x)
             {
                 answer = true;
             }
             // end
-
             return answer;
         }
 
         public bool Task7(int n)
         {
             bool answer = true;
-
             // code here
             if (n < 0)
             {
@@ -108,13 +95,11 @@ namespace Lab1
                 answer = false;
             }
             // end
-
             return answer;
         }
         public bool Task8(int X, int Y)
         {
             bool answer = false;
-
             // code here
             int k = 1;
             int s = 0;
@@ -140,8 +125,8 @@ namespace Lab1
                 answer = false;
             }
                 // end
-
                 return answer;
         }
     }
 }
+
